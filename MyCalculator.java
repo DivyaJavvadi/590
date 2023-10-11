7ad8e0fa14fb2c58cd12740a43fdd6e1eea4598f
admin <admin@admin-PC>
2023-10-11 10:04:21 +0530

    color
    Change-Id: I4f9bd076f116bf4815e5a159781e514d745cb2d4

--- MyCalculator.java
+++ MyCalculator.java
@@ -41,7 +41,7 @@ super(frameText);
 int tempX=TOPX, y=TOPY;
 displayLabel.setBounds(tempX,y,240,HEIGHT);
 displayLabel.setBackground(Color.BLUE);
-displayLabel.setForeground(Color.WHITE);
+displayLabel.setForeground(Color.GREEN);
 add(displayLabel);
 
 memLabel.setBounds(TOPX,  TOPY+HEIGHT+ V_SPACE,WIDTH, HEIGHT);
